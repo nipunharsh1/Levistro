315b41e42ef4e6b2bb620497d564860f6cdab7db
Gamage N.H. <127107611+nipunharsh1@users.noreply.github.com>
2026-09-17 06:29:28 +0530

    add file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Use class strategy for dark mode so we can toggle it from JS
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        '8xl': ['6rem', { lineHeight: '1' }],
+        '9xl': ['8rem', { lineHeight: '1' }],
+      },
+    },
+  },
+  plugins: [],
+};
